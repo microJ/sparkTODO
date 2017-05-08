4291ef928304c75aa5a351052623a179db64508a
xiaoJay <oh521hero@163.com>
2017-05-09 00:55:24 +0800

    update

--- dmeo/todo.js
+++ dmeo/todo.js
@@ -1,9 +1,20 @@
-var list = [
+var store = {
+    save(key,value){
+        localStorage.setItem(key,JSON.stringify(value))
+    },
+    fetch(key){
+        return JSON.parse(localStorage.getItem(key)) || []
+    }
+}
+
+/*var list = [
     {title: '你好啊',
     isChecked: false},
     {title: '我很好啊',
     isChecked: true}
-]
+]*/
+var list = store.fetch('sparkTODO-list')
+
 new Vue({
     el: '.main',
     data: {
@@ -11,6 +22,14 @@ new Vue({
         list: list,
         editorTodos: '',
         beforeTitle: ''
+    },
+    watch: {
+        list:{
+            handler: function(){
+                store.save('sparkTODO-list',this.list)
+            },
+            deep: true
+        }
     },
     computed: {
         noCheckLength: function () {
